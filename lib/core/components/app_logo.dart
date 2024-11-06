d9afd9a52534e567caeca26026132b3bfa48f1da
DUDU0123 <sdu200115@gmail.com>
2024-11-06 12:22:04 +0530

    User authentication completed.

--- lib/core/components/app_logo.dart
+++ lib/core/components/app_logo.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 Widget appLogo() {
     return Container(
-      width: 200,
       height: 200,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
@@ -10,6 +9,7 @@ Widget appLogo() {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(15),
         child: Image.asset(
+          scale: 1,
           "assets/contact_box.png",
           fit: BoxFit.cover,
         ),
--- lib/features/auth/presentation/widgets/google_sigin_button.dart
+++ lib/features/auth/presentation/widgets/google_sigin_button.dart
@@ -2,7 +2,6 @@ import 'package:contact_box/core/components/text_widget_common.dart';
 import 'package:contact_box/core/constants/colors.dart';
 import 'package:contact_box/core/constants/height_width.dart';
 import 'package:contact_box/features/auth/presentation/bloc/bloc/authentication_bloc.dart';
-import 'package:contact_box/features/main_entry/presentation/bloc/cubit/page_select_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
@@ -19,27 +18,31 @@ class GoogleSignInButton extends StatelessWidget {
         context.read<AuthenticationBloc>().add(SignInUserEvent());
       },
       child: Container(
-        padding:const  EdgeInsets.symmetric(vertical: 10),
+        padding: const EdgeInsets.symmetric(vertical: 10),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: kBlack,
-          boxShadow:   [
-            BoxShadow(
-              color: kWhite.withOpacity(0.4),
-              blurRadius: 1,spreadRadius: 1,
-              offset:const Offset(1, 0),
-            )
-          ]
-        ),
+            borderRadius: BorderRadius.circular(10),
+            color: kBlack,
+            boxShadow: [
+              BoxShadow(
+                color: kWhite.withOpacity(0.4),
+                blurRadius: 1,
+                spreadRadius: 1,
+                offset: const Offset(1, 0),
+              )
+            ]),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SvgPicture.network(
-              height: 40, width: 40,
+              height: 40,
+              width: 40,
               "https://static.cdnlogo.com/logos/g/38/google-icon.svg",
             ),
             kWidth15,
-            const TextWidgetCommon(text: "Sign In With Google", ),
+            const TextWidgetCommon(
+              text: "Sign In With Google",
+              fontWeight: FontWeight.bold,
+            ),
           ],
         ),
       ),
